9e333ad782864314ec19c41e04de174bae0c2fca
zeta-squared <zeaiter.zeaiter@hotmail.com>
2024-11-19 12:12:29 +1100

    cleaned structure

--- Chapter 2/sum decomposition/binary_search.h
+++
@@ -1,6 +0,0 @@
-#ifndef BINARY_SEARCH_H
-#define BINARY_SEARCH_H
-
-int binary_search(int A[], int v, int l, int s);
-
-#endif
--- Chapter 2/sum decomposition/merge_sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef MERGE_SORT_H
-#define MERGE_SORT_H
-
-void merge_sort(int A[], int p, int r);
-void merge(int A[], int p, int q, int r);
-
-#endif
